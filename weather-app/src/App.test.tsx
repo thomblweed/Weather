d2f00e8f6f37dfd600cfb1f67a97b0cdc9523c7c
Thomas Newman <thomas.newman@company-net.com>
2018-05-14 20:58:50 +0100

    initial testing of App component

--- weather-app/src/App.test.tsx
+++ weather-app/src/App.test.tsx
@@ -1,9 +1,33 @@
+// react and component
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import App from './App';
+// setup
+import { configure } from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+// test
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+
+  // check we have an App component
+  it('should render <App /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(App));
+  }); 
+
+  // state value checks
+  describe('    checking state values on first render', ()=> {
+    it('  "weatherData" state should be null', ()=> {
+      const wrapper = shallow(<App />);
+      expect(wrapper.state().weatherData).toEqual(null);
+    });
+    it('  "error" state should be false', ()=> {
+      const wrapper = shallow(<App />);
+      expect(wrapper.state().error).toEqual(false);
+    });
+  });
+
+  // check fetch
+
 });
